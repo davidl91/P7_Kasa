11541625c6949d74962bc46431cf689bb04c4416
davidl91 <davidlencel91@gmail.com>
2023-02-13 19:13:54 +0100

    Modification des imports datas par des API Fetch

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,26 @@
-import React from "react"
-import datas from "../../datas/logements.json"
+import React, {useState, useEffect} from "react"
 import Banner from "../../components/banner"
 import Card from "../../components/card"
 import bannerhome from "../../assets/banner-home.png"
 
 function Home() {
+  const [datas, setDatas] = useState([])
+
+  useEffect(() => {
+    fetch("./logements.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        setDatas(data)
+      })
+  }, [])
+
   return (
     <>
       <Banner image={bannerhome} texte="Chez vous, partout et ailleurs" />
--- src/pages/Location/index.jsx
+++ src/pages/Location/index.jsx
@@ -1,6 +1,5 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import {useParams, Navigate} from "react-router-dom"
-import datas from "../../datas/logements.json"
 import Gallery from "../../components/gallery"
 import Tag from "../../components/tag"
 import Profil from "../../components/profil"
@@ -8,30 +7,55 @@ import Notation from "../../components/notation"
 import Collapse from "../../components/collapse"
 
 function Location() {
-  const locationId = useParams()
-  const location = datas.find((data) => data.id === locationId.id)
+  const {id} = useParams()
+  const [datas, setDatas] = useState({
+    pictures: [],
+    title: "",
+    location: "",
+    tags: [],
+    host: {name: "", picture: ""},
+    rating: "",
+    description: "",
+    equipments: [],
+  })
 
-  if (location === undefined) {
+  useEffect(() => {
+    fetch("../logements.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        const location = data.find((data) => data.id === id)
+        setDatas(location)
+      })
+  }, [id])
+
+  if (datas === undefined) {
     return <Navigate replace to="/Error404" />
   }
 
   return (
     <>
-      <Gallery image={location.pictures} />
+      <Gallery image={datas.pictures} />
       <section className="location">
         <div className="location__text">
-          <h1 className="location__title">{location.title}</h1>
-          <p className="location__paragraph">{location.location}</p>
-          <Tag texte={location.tags} />
+          <h1 className="location__title">{datas.title}</h1>
+          <p className="location__paragraph">{datas.location}</p>
+          <Tag texte={datas.tags} />
         </div>
         <div className="location__info">
-          <Profil texte={location.host.name} image={location.host.picture} />
-          <Notation note={location.rating} />
+          <Profil texte={datas.host.name} image={datas.host.picture} />
+          <Notation note={datas.rating} />
         </div>
       </section>
       <section className="toggles">
-        <Collapse title="Description" description={location.description} />
-        <Collapse title="Équipements" description={location.equipments} />
+        <Collapse title="Description" description={datas.description} />
+        <Collapse title="Équipements" description={datas.equipments} />
       </section>
     </>
   )
